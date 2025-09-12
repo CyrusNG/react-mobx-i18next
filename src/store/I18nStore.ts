65181eda3ad688d92e6b6108a7426c225033c29f
CyrusNG <wu.cyrus@gmail.com>
2025-09-12 20:51:30 +0800

    use @observable in I18nStore instead of makeAutoObservable

--- src/store/I18nStore.ts
+++ src/store/I18nStore.ts
@@ -1,22 +1,20 @@
-import { makeAutoObservable } from 'mobx'
+import { observable } from 'mobx'
 import type { i18n } from 'i18next'
 
 /**
 * MobX store for current language. Changing locale will call i18next.changeLanguage.
 */
 export class I18nStore {
-  locale: string
-  private i18n: i18n
+  @observable locale: string
+  @observable private i18n: i18n
 
   constructor(i18n: i18n) {
     this.i18n = i18n
     this.locale = i18n.language || 'en'
-    makeAutoObservable(this)
   }
 
-  setLocale = async (lng: string) => {
-    if (this.locale === lng) return
-    this.locale = lng
-    await this.i18n.changeLanguage(lng)
+  setLocale = async (lang: string) => {
+    if (this.locale === lang) return this.locale = lang
+    await this.i18n.changeLanguage(lang)
   }
 }
